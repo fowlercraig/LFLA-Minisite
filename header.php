41325ab8dd2718f598bdc3639d534087c6ba4965
Craig Fowler <craig@wearegiants.com>
2015-05-28 14:28:03 -0700

    Initial Commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<title><?php wp_title(' | ', true, 'right'); ?></title>
+<meta name="description" content="<?php bloginfo( 'description' ) ?>">
+<link rel="shortcut icon" href="/assets/img/favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<?php wp_head(); ?>
+</head>
+
+<?php
+  $menuParameters = array(
+    'container'       => false,
+    'echo'            => false,
+    'items_wrap'      => '%3$s',
+    'theme_location'  => 'main-menu',
+    //'Walker_Nav_Menu' => new NavWalker(),
+    'depth'           => 0,
+  );
+?>
+
+<body <?php body_class('fs-grid'); ?>>
