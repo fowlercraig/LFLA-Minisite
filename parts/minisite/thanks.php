5ce8c94a405486aa5cf883a6c03a34d7cb0b9257
Craig Fowler <craig@wearegiants.com>
2015-06-02 13:45:39 -0700

    mobile changes.

--- parts/minisite/thanks.php
+++ parts/minisite/thanks.php
@@ -1,6 +1,6 @@
 <h3 class="section-title"><?php the_field('section_title_thanks'); ?></h3>
 <div class="fs-row">
-  <div id="home--thanks-first"  class="fs-cell fs-lg-12 fs-md-6 fs-sm-3 text-center"><div class="wrapper"><?php the_field('main_thanks'); ?></div></div>
-  <div id="home--thanks-second" class="fs-cell fs-lg-6  fs-md-3 fs-sm-3 equal text-center"><div class="wrapper"><?php the_field('main_thanks_second'); ?></div></div>
-  <div id="home--thanks-third"  class="fs-cell fs-lg-6  fs-md-3 fs-sm-3 equal text-center"><div class="wrapper"><?php the_field('main_thanks_third'); ?></div></div>
+  <div id="home--thanks-first"  class="fs-cell fs-lg-12 fs-md-6 fs-sm-3    fs-xs-3 text-center"><div class="wrapper"><?php the_field('main_thanks'); ?></div></div>
+  <div id="home--thanks-second" class="fs-cell fs-lg-6  fs-md-3 fs-sm-half fs-xs-3 equal text-center"><div class="wrapper"><?php the_field('main_thanks_second'); ?></div></div>
+  <div id="home--thanks-third"  class="fs-cell fs-lg-6  fs-md-3 fs-sm-half fs-xs-3 equal text-center"><div class="wrapper"><?php the_field('main_thanks_third'); ?></div></div>
 </div>
