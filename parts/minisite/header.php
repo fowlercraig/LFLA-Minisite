1457ae1e19de8a54d0c1b587c2db0b4cd211986f
Craig Fowler <craig@wearegiants.com>
2015-06-03 11:37:37 -0700

    Added 'visible' toggle to links

--- parts/minisite/header.php
+++ parts/minisite/header.php
@@ -1,7 +1,7 @@
 <div class="fs-row">
   <div class="fs-cell fs-lg-12 fs-md-hide fs-sm-hide text-center fs-centered">
     <?php if( have_rows('navigation') ): $i = 0; while ( have_rows('navigation') ) : the_row(); ?>
-      <a href="<?php the_sub_field('nav_url'); ?>" class="btn btn-nav"><?php the_sub_field('nav_label'); ?></a>
+      <?php if(get_sub_field('visible')): ?><a href="<?php the_sub_field('nav_url'); ?>" class="btn btn-nav"><?php the_sub_field('nav_label'); ?></a><?php endif; ?>
     <?php endwhile; endif; ?>
   </div>
   <div class="fs-cell fs-lg-hide fs-md-5 fs-sm-2">
