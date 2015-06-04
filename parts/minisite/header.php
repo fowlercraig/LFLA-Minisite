41285072287e10a93b1f643dd27d067d4f972f10
Craig Fowler <craig@wearegiants.com>
2015-06-03 19:22:22 -0700

    Added logo to mobile

--- parts/minisite/header.php
+++ parts/minisite/header.php
@@ -5,14 +5,15 @@
     <?php endwhile; endif; ?>
   </div>
   <div class="fs-cell fs-lg-hide fs-md-5 fs-sm-2">
+    <a href="http://lfla.org"id="home--header-logo" class="btn btn-nav btn-first">The Library Foundation of Los Angeles</a>
     <a href="#home--wrapper" class="btn btn-nav">To Live and Dine in LA</a>
   </div>
   <div class="fs-cell fs-lg-hide fs-md-1 fs-sm-1 fs-right">
     <a id="menuBtn" class="btn btn-nav btn-last text-right" href="#">
       <span>
-        <span>yo</span>
-        <span>asdf</span>
-        <span>as</span>
+        <span></span>
+        <span></span>
+        <span></span>
       </span>
     </a>
   </div>
