68e76ce64a063917ff03636025b91496cf9600e9
Craig Fowler <craig@wearegiants.com>
2015-06-03 19:46:45 -0700

    Added main logo

--- parts/minisite/header.php
+++ parts/minisite/header.php
@@ -1,3 +1,4 @@
+<a id="home--header-logo" href="http://lfla.org" target="blank">The Library Foundation of Los Angeles</a>
 <div class="fs-row">
   <div class="fs-cell fs-lg-12 fs-md-hide fs-sm-hide text-center fs-centered">
     <?php if( have_rows('navigation') ): $i = 0; while ( have_rows('navigation') ) : the_row(); ?>
@@ -5,7 +6,7 @@
     <?php endwhile; endif; ?>
   </div>
   <div class="fs-cell fs-lg-hide fs-md-5 fs-sm-2">
-    <a href="http://lfla.org"id="home--header-logo" class="btn btn-nav btn-first">The Library Foundation of Los Angeles</a>
+    <a href="http://lfla.org"id="home--header-logo-mobile" class="btn btn-nav btn-first" target="blank">The Library Foundation of Los Angeles</a>
     <a href="#home--wrapper" class="btn btn-nav">To Live and Dine in LA</a>
   </div>
   <div class="fs-cell fs-lg-hide fs-md-1 fs-sm-1 fs-right">
